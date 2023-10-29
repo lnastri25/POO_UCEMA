893b390f2b30064ab539f51bc8624c76ea3eac63
Lorenzo Nastri <lorenzonastri@gmail.com>
2023-10-29 20:28:42 -0300

    update

--- trabajos_practicos/tp_datos/2-ecommerce/olist/preprocess.py
+++ trabajos_practicos/tp_datos/2-ecommerce/olist/preprocess.py
@@ -1,6 +1,39 @@
 import pandas as pd
 import numpy as np
 
+
+
+def whitespace_remover(dataframe):
+    # iterating over the columns
+    for i in dataframe.columns:
+        # checking datatype of each column
+        if dataframe[i].dtype == 'object':
+            # applying strip function on column
+            dataframe[i] = dataframe[i].apply(lambda x: x.strip() if isinstance(x, str) else x)
+    return dataframe
+
+
+def whitespace_remover_and_columns(dataframe):
+   
+    # iterating over the columns
+    for i in dataframe.columns:
+         
+        # checking datatype of each columns
+        if dataframe[i].dtype == 'object':
+             
+            # applying strip function on column
+            dataframe[i] = dataframe[i].map(str.strip)
+        else:
+             
+            # if condn. is False then it will do nothing.
+            pass
+    dataframe.rename(columns=lambda x: x.strip(), inplace=True)
+    return dataframe
+
+
+
+
+
 ## PUNTO A) ##
 
 # Función para transformar columnas de un dataset a datetime.
@@ -19,26 +52,39 @@ def calcular_time_delta(df, columna_fecha_inicio, columna_fecha_fin):
     return df
 
 
-# Función para calcular el tiempo de entrega.
-def tiempo_de_espera(orders, is_delivered=True):
+# Función para calcular el tiempo de entrega REAL.
+def tiempo_de_espera_real(orders, is_delivered=True):
     # filtrar por entregados y crea la varialbe tiempo de espera
     if is_delivered:
         orders = orders.query("order_status=='delivered'").copy()
     # compute wait time
-    orders.loc[:, 'tiempo_de_espera'] = \
+    orders.loc[:, 'tiempo_de_espera_real'] = \
         (orders['order_delivered_customer_date'] -
          orders['order_purchase_timestamp']) / np.timedelta64(24, 'h')
     return orders
 
 
+# Función para calcular el tiempo de entrega ESPERADO.
+def tiempo_de_espera_esperado(orders, is_delivered=True):
+    # filtrar por entregados y crea la varialbe tiempo de espera
+    if is_delivered:
+        orders = orders.query("order_status=='delivered'").copy()
+    # compute wait time
+    orders.loc[:, 'tiempo_de_espera_esperado'] = \
+        (orders['order_estimated_delivery_date'] -
+         orders['order_purchase_timestamp']) / np.timedelta64(24, 'h')
+    return orders
+
+
 # Función para calcular la diferencia entre la fecha de entrega y la fecha de entrega estimada. si la fecha de entrega real es posterior a la fecha de entrega estimada, devuelve el número de días entre las dos fechas; de lo contrario, devuelve 0
-def real_vs_esperado(df, is_delivered=True):
-    # Calculo la diferencia entre la fecha de entrega y la fecha de entrega estimada
-    df['real_vs_esperado'] = df['order_delivered_carrier_date'] - df['order_estimated_delivery_date']
-    # Reemplazo los valores negativos por 0
-    df['real_vs_esperado'] = df['real_vs_esperado'].apply(lambda x: x.days)
-    df['real_vs_esperado'] = df['real_vs_esperado'].apply(lambda x: 0 if x < 0 else x)
-    return df
+def real_vs_esperado(orders, is_delivered=True):
+    if is_delivered:
+        orders = orders.query("order_status=='delivered'").copy()
+        
+    orders['real_vs_esperado'] = np.where(orders['tiempo_de_espera_real'] > orders['tiempo_de_espera_esperado'], 
+                                           orders['tiempo_de_espera_real'] - orders['tiempo_de_espera_esperado'], 
+                                           0)
+    return orders
 
 
 
@@ -46,31 +92,44 @@ def real_vs_esperado(df, is_delivered=True):
 
 # Función para calcular el puntaje de compra. Entre 1 y 5
 def review_score(df):
-    df['review_score'] = df['review_score'].apply(lambda x: 1 if x < 3 else 2 if x < 4 else 3 if x < 5 else 4)
-    return df
+    def es_cinco_estrellas(x):
+        if x == 5:
+            return 1
+        else:
+            return 0 
+    df['es_cinco_estrellas'] = df['review_score'].apply(es_cinco_estrellas)
+    
+    def es_una_estrella(x):
+        if x == 1:
+            return 1 
+        else:
+            return 0
+    df['es_una_estrella'] = df['review_score'].apply(es_una_estrella)
+    
+    return df[['order_id','es_cinco_estrellas', 'es_una_estrella', 'review_score']]
 
 
 
 ## PUNTO C) ##
 
 # Función para calcular el número de productos por orden.
-def calcular_numero_de_productos(orders_items, products):
-    # Hago un merge entre los dfs usando la columna 'product_id'
-    merged_data = orders_items.merge(products, on='product_id', how='inner')
-    # Agrupo por 'order_id' y cuento el número de productos por orden
-    product_count_per_order = merged_data.groupby('order_id')['product_id'].count().reset_index()
-    product_count_per_order.rename(columns={'product_id': 'number_of_products'}, inplace=True)
-    return product_count_per_order
+def calcular_numero_de_productos(df):
+    df1 = df.copy()
+    order_items_df = df1['order_items'].copy()
+    order_items_df = whitespace_remover(order_items_df)
+    order_items_df.rename(columns=lambda x: x.strip(), inplace=True)
+    return order_items_df.groupby('order_id').agg(num_de_produc = ('product_id','count')).reset_index()
 
 
 
 ## PUNTO D) ## 
 
-def vendedores_unicos(orders_items):
-    # Agrupa por 'order_id' y obtiene el número de vendedores únicos por orden.
-    vendedores_unicos_por_orden = orders_items.groupby('order_id')['seller_id'].nunique().reset_index()
-    vendedores_unicos_por_orden.rename(columns={'seller_id': 'vendedores_unicos'}, inplace=True)
-    return vendedores_unicos_por_orden
+def vendedores_unicos(df):
+    df2 = df.copy()
+    oitp4 = df2['order_items'].copy()
+    oitp4 = whitespace_remover(oitp4)
+    oitp4.rename(columns=lambda x: x.strip(), inplace=True)
+    return oitp4.groupby('order_id').agg(vendedores_unicos = ('seller_id','nunique')).reset_index()
 
 '''
 def contar_valores_unicos_por_grupo(data, group_column, value_column):
@@ -86,20 +145,19 @@ def contar_valores_unicos_por_grupo(data, group_column, value_column):
 ## PUNTO E) ## 
 
 # Funcion para calcular el precio y el transporte de la order_id
-def calcular_precio_y_transporte(order_items, productos):
-    # Fusiona los DataFrames usando la columna 'product_id'
-    merged_data = order_items.merge(productos, on='product_id', how='inner')
-    # Agrupa por 'order_id' y calcula el precio y el transporte de la orden
-    precio_y_transporte = merged_data.groupby('order_id').agg(precio = ('price', 'sum'), transporte = ('freight_value', 'sum')).reset_index()
-    return precio_y_transporte
+def calcular_precio_y_transporte(df):
+    df2=df.copy()
+    order_items_df2 = df2['order_items'].copy()
+    order_items_df2 = whitespace_remover(order_items_df2)
+    order_items_df2.rename(columns=lambda x: x.strip(), inplace=True)
+    return order_items_df2.groupby('order_id').agg(precio = ('price','sum'), transporte=('freight_value','sum')).reset_index()
 
 
 
 ## PUNTO F) ## 
 
 from math import radians, sin, cos, asin, sqrt
-
-def calcular_distancia_vendedor_comprador(lon1, lat1, lon2, lat2):
+def haversine_distance(lon1, lat1, lon2, lat2):
     """
     Computa distancia entre dos pares (lat, lng)
     Ver - (https://en.wikipedia.org/wiki/Haversine_formula)
@@ -109,3 +167,12 @@ def calcular_distancia_vendedor_comprador(lon1, lat1, lon2, lat2):
     dlat = lat2 - lat1
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     return 2 * 6371 * asin(sqrt(a))
+
+def calculate_distance(row):
+    return haversine_distance(row['geolocation_lng_x'], row['geolocation_lat_x'], row['geolocation_lng_y'], row['geolocation_lat_y'])
+
+
+def crear_columna(df):
+    df2=df.copy()
+    df2['distancia_entre_cliente_y_vendedor'] = df2.apply(calculate_distance, axis=1)
+    return df2[['order_id', 'distancia_entre_cliente_y_vendedor']]
